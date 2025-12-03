412bd0d63aba8dbc478b641695d8f9daf2f02f77
Maxxow <maximiliano.mtnz.gnlz@gmail.com>
2025-12-02 23:44:27 -0600

    Leccion 2 de tipos de variables

--- Data_Type/src/main.rs
+++ Data_Type/src/main.rs
@@ -1,7 +1,34 @@
 fn main() {
-    let mut y = 10; // Mutable
-    println!("El valor de y es: {}", y);
+    // Variables
+    // Se define una variable inmutable para el ID del objetivo
+    let id_objetivo: i32 = 101;
 
-    y = 15; // ¡Esto ahora es válido!
-    println!("El nuevo valor de y es: {}", y);
+    // Definimos una variable MUTABLE para el estado de la conexion 
+    // Inicialmente sera un "false"
+    let mut conectado: bool = false;
+
+    println!("Objetivo : Sistema #{}", id_objetivo);
+    println!("Estado inicial de conexion: {}", conectado);
+
+    // 2 Cambiando valores
+    // Como 'conectado' es 'mut' podemos cambiarla
+    println!("Iniciando conexion -----------");
+    conectado = true;
+    println!("Estado actual de conexion: {}", conectado);
+
+    // 3.- Usando funciones
+    // Llamamos un funcion que calcule el el riesgo
+    let vulnerabilidades = 5;
+    let riesgo =  calcular_riesgo(vulnerabilidades);
+
+    println!("Vulnerabilidades detectadas {}", vulnerabilidades);
+    println!("Nivel de riesgo calculado {}", riesgo);
+
+}
+
+// Aqui vamos a declarar una funcion para poder llamarla en la principal
+fn calcular_riesgo(cantidad_vulns: i32){
+    let factor_riesgo = 2;
+
+    cantidad_vulns * factor_riesgo;
 }
