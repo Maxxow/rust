0f084c1d3fe63fe1e4802cb1e5c18c22c22ec3ab
Maxxow <maximiliano.mtnz.gnlz@gmail.com>
2025-12-03 21:31:11 -0600

    Arrays and Tuplas in rus

--- Data_Type/src/main.rs
+++ Data_Type/src/main.rs
@@ -24,6 +24,38 @@ fn main() {
     println!("Vulnerabilidades detectadas {}", vulnerabilidades);
     println!("Nivel de riesgo calculado {}", riesgo);
 
+    // *--- Nuevo Codgio--------
+    // *Definiremos un arreglo de puertos comunes (Son enteros i32)
+    // *[i32; 4] =  significa "Tipo i32, tamaño 4"
+    let comon_ports: [i32;4]= [80,443,22,21];
+
+    // *Accedemos ficticiamente al puerto SSH(El indice comienza en  0)
+    // *Indice 0 = 8 ....
+    let ssh_port = comon_ports[2];
+    println!("Escaneando el puerto SSH {}", ssh_port);
+
+    // * Ejemplo ficticio
+    // Buffer de memoria llena (Muy comun en exploits)
+    // Esto crea un arreglo de 5 ceros : [0,0,0,0,0]
+    let buffer = [0; 5];
+    println!("Buffer de memoria inicializado : {:?}", buffer);      // Aqui usamos el :? entre parentesis para poder imprimir estructuras complejas como un array
+
+
+    // --- LO QUE FALTABA: TUPLAS ---
+    
+    // Una tupla nos permite agrupar datos de DIFERENTE tipo.
+    // Ejemplo: (Dirección IP, Puerto, Protocolo Activo)
+    //         (String,       i32,    bool)
+    let credencials: (&str, i32, bool) = ("192.168.0.50", 800, true);
+
+    // Para acceder a los datos  de una tupla usamos el punto "."
+    println!("--Datos del servidor--");
+    println!("IP Objetivo {}", credencials.0);
+    println!("Puerto : {}", credencials.1);
+
+    // Desestructuracion: Sacar todos los valores a variables sueltas
+    let (ip, puerto, estado)= credencials;
+    println!("El servidor {} en el puerto {} esta activo: {}", ip, puerto, estado);
 }
 
 // Aqui vamos a declarar una funcion para poder llamarla en la principal
